a53ce3c5f2df15ed0912ba1e509717787c9bdfb2
Yehor DZHURYNSKYI <ydzhuryn@e1r3p4.unit.ua>
2020-03-09 19:25:16 +0200

    removed unnec files

--- src/md5/md5.mk
+++ src/md5/md5.mk
@@ -2,7 +2,6 @@ MD5_DIR		:=	$(FT_SSL_DIR)src/md5/
 MD5_SRC		:=	md5.c\
 				md5_funcs.c\
 				md5_rounds.c\
-				#md5_provider.c\
 
 MD5_OBJ		=	$(addprefix $(FT_SSL_OBJ_DIR), $(MD5_SRC:.c=.o))
 FT_SSL_SRC	+=	$(addprefix $(MD5_DIR), $(MD5_SRC:.c=.o))
